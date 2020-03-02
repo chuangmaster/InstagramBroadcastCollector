19c996edbfe1023e0a549d6a05b373deda615f3d
chuangmaster <masterchuang@urtody.com>
2020-03-02 10:18:14 +0800

    json format

--- InstagramBroadcastCollector/Models/InstagramCommentDTO.cs
+++ InstagramBroadcastCollector/Models/InstagramCommentDTO.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace InstagramBroadcastCollector.Models
 {
     public class InstagramCommentDTO
     {
         public long Pk { get; set; }
+        [JsonProperty("name")]
         public string UserName { get; set; }
+        [JsonProperty("comment")]
         public string Text { get; set; }
         public DateTime CreatedTime { get; set; }
     }
