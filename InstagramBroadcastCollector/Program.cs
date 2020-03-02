05725a24042626b127768a64380c42661971410b
chuangmaster <masterchuang@urtody.com>
2020-03-02 14:33:34 +0800

    Add verification and set bin file

--- InstagramBroadcastCollector/Program.cs
+++ InstagramBroadcastCollector/Program.cs
@@ -3,15 +3,20 @@ using InstagramApiSharp.API.Builder;
 using InstagramApiSharp.Classes;
 using InstagramApiSharp.Logger;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using InstagramApiSharp.Classes.SessionHandlers;
+using System.IO;
 
 namespace InstagramBroadcastCollector
 {
     class Program
     {
+        const string StateFile = "state.bin";
+
         async static Task Main(string[] args)
         {
             Console.Write("Input your instagram UserName:");
@@ -49,20 +54,138 @@ namespace InstagramBroadcastCollector
                 .SetUser(userSession)
                 .UseLogger(new DebugLogger(LogLevel.All))
                 .SetRequestDelay(delay)
+                // Session handler, set a file path to save/load your state/session data
+                .SetSessionHandler(new FileSessionHandler() { FilePath = StateFile })
                 .Build();
+            //Load session
+            try
+            {
+                if (File.Exists(StateFile))
+                {
+                    Console.WriteLine("Loading state from file");
+                    using (var fs = File.OpenRead(StateFile))
+                    {
+                        api.LoadStateDataFromStream(fs);
+                        // in .net core or uwp apps don't use LoadStateDataFromStream
+                        // use this one:
+                        // _instaApi.LoadStateDataFromString(new StreamReader(fs).ReadToEnd());
+                        // you should pass json string as parameter to this function.
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
             if (!api.IsUserAuthenticated)
             {
                 // login
                 Console.WriteLine($"Logging in as {userSession.UserName}");
                 delay.Disable();
-                var logInResult = await api.LoginAsync();
+                var logInResult = await api.LoginAsync(false);
                 delay.Enable();
                 if (!logInResult.Succeeded)
                 {
                     Console.WriteLine($"Unable to login: {logInResult.Info.Message}");
+
+                    if (logInResult.Value == InstaLoginResult.ChallengeRequired)
+                    {
+                        var challenge = await api.GetChallengeRequireVerifyMethodAsync();
+                        if (challenge.Succeeded)
+                        {
+                            if (challenge.Value.SubmitPhoneRequired)
+                            {
+                                Console.WriteLine("input your phone number:");
+                                var phoneNumber = Console.ReadLine();
+                                var submitPhone = await api.SubmitPhoneNumberForChallengeRequireAsync(phoneNumber);
+                                if (submitPhone.Succeeded)
+                                {
+                                    var verifiedResult = await VerifiedCodeAsync(api);
+                                    Console.WriteLine(verifiedResult ? "Login success!" : "Login fail!");
+                                }
+                                else
+                                {
+                                    throw new Exception("Submit phone number error...");
+                                }
+                            }
+                            else
+                            {
+                                if (challenge.Value.StepData != null)
+                                {
+                                    Dictionary<int, string> verifyMethod = new Dictionary<int, string>();
+                                    if (!string.IsNullOrEmpty(challenge.Value.StepData.PhoneNumber))
+                                    {
+                                        verifyMethod.Add(1, "phone");
+                                    }
+                                    if (!string.IsNullOrEmpty(challenge.Value.StepData.Email))
+                                    {
+                                        verifyMethod.Add(2, "email");
+                                    }
+
+                                    Console.WriteLine("Select one to get yor verification code:");
+                                    foreach (var m in verifyMethod)
+                                    {
+                                        Console.WriteLine($"Press {m.Key} to use {m.Value}");
+                                    }
+
+                                    var key = Console.ReadLine();
+                                    if (key == "1")
+                                    {
+                                        // send verification code to phone number
+                                        var phoneNumber = await api.RequestVerifyCodeToSMSForChallengeRequireAsync();
+                                        var verifiedResult = await VerifiedCodeAsync(api);
+                                        Console.WriteLine(verifiedResult ? "Login success!" : "Login fail!");
+
+                                    }
+                                    else if (key == "2")
+                                    {
+                                        // send verification code to email
+                                        var email = await api.RequestVerifyCodeToEmailForChallengeRequireAsync();
+                                        var verifiedResult = await VerifiedCodeAsync(api);
+                                        Console.WriteLine(verifiedResult ? "Login success!" : "Login fail!");
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Challenge error");
+                        }
+                    }
                 }
+            }
+            var state = api.GetStateDataAsStream();
+            // in .net core or uwp apps don't use GetStateDataAsStream.
+            // use this one:
+            // var state = _instaApi.GetStateDataAsString();
+            // this returns you session as json string.
+            using (var fileStream = File.Create(StateFile))
+            {
+                state.Seek(0, SeekOrigin.Begin);
+                state.CopyTo(fileStream);
             }
             return api;
         }
+
+        /// <summary>
+        /// verified code
+        /// </summary>
+        /// <param name="api"></param>
+        /// <returns></returns>
+        private static async Task<bool> VerifiedCodeAsync(IInstaApi api)
+        {
+            Console.WriteLine("Input your verified code:");
+            var code = Console.ReadLine();
+            var verifyLogin = await api.VerifyCodeForChallengeRequireAsync(code);
+            if (verifyLogin.Succeeded)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
+
